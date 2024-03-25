b4f97438dd70559b2419ede7f8efaa43978fbcd3
anees786687 <78203712+anees786687@users.noreply.github.com>
2024-03-25 13:25:41 +0530

    Update Question_1.cpp

--- Recursion/assignmet_1/Question_1.cpp
+++ Recursion/assignmet_1/Question_1.cpp
@@ -1,1 +1,17 @@
+/**Write a program to calculate the sum of odd numbers between a and b (both inclusive) using
+recursion.*/
 
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+int recSum(int a, int b){
+    if(a > b) return 0;
+    if(a % 2 == 0) a += 1;
+    return a + recSum(a+1,b);
+}
+int main(){
+    cout << recSum(1,7) << endl;
+}
